ef1ad9863e5bdf058d11018212516fd85b3baf9f
Luna Cleary <lunacleary@outlook.com>
2023-06-15 13:35:06 +0200

    changed how modifying values works to p works to prevent segfault, not currently possible to put char data into a cell

--- duvet.c
+++ duvet.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<ctype.h>
 #include<stdlib.h>
 #define ROWS 20
@@ -20,6 +21,19 @@ int cellpicker(int row, int column){
 	return activeCell;
 }
 
+void cellModifier(int activeCell){
+	int intInput;
+	printf("input your data:");
+	scanf("%d", &intInput);
+	cellsArray[activeCell].numberData = intInput; 
+
+	return;
+	}
+
+void cellReader(int activeCell){
+	printf("the cell contains the integer %d\n", cellsArray[activeCell].numberData);
+		return;
+}
 int main(void){
 	int activeRow;
 	int activeColumn;
@@ -27,6 +41,7 @@ int main(void){
 		cellsArray[i].id = i;
 		cellsArray[i].charData = malloc(32 * sizeof(char)); //gives every cell a unique identifier, and allocates 32 bytes of memory for character data per cell
 	}
+	while(1){
 	printf("Choose your row:");
 		scanf("%d", &activeRow);
 	printf("Choose your column:");
@@ -34,21 +49,25 @@ int main(void){
 		int activeCell = cellpicker(activeRow, activeColumn);
 //	printf("%d", activeCell); /*just making sure cellpicker works*/
 	char modifyOrRead; 
-	printf("What would you like to do? (m)odify value/(r)ead value: ");
+	printf("What would you like to do? (m)odify value/(r)ead value/(q)uit program: ");
 	scanf(" %c", &modifyOrRead);
 		modifyOrRead = tolower(modifyOrRead);
-			if (modifyOrRead != ('m') && (modifyOrRead != 'r')){
+			if (modifyOrRead != ('m') && (modifyOrRead != 'r' && (modifyOrRead != 'q'))){
 
 			printf("error, must enter m or r\n");
 				return -1;
 			}
 			else if (modifyOrRead == ('m')){
-			scanf("%s", &cellsArray[activeCell].charData);
-			printf("%s", &cellsArray[activeCell].charData); /*debugging*/
-
+			cellModifier(activeCell);
+			
+			}
+			else if (modifyOrRead == ('q')){
+				break;
 			}
 			else{ 
-					printf("read");
+				cellReader(activeCell);
 				}
+	}
+
 	return 0;
 }
