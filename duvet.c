bb106e154421c9d69fdc9f72cdef99ebf5c0c65d
Luna Cleary <lunacleary@outlook.com>
2023-07-29 21:20:56 +0200

    got rid of some magic numbers

--- duvet.c
+++ duvet.c
@@ -1,21 +1,22 @@
+//TODO: add a way to visualize spreadsheet
+//TODO: before doing that, make it possible to see which cells have data in them
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
 #include<stdlib.h>
-#include<string.h>
 #define ROWS 20
 #define COLUMNS 20 
+
 //define ANSI escape codes, these are there to make certain output in different colors, so the output is more readable
 #define ANSI_COLOR_MAGENTA "\x1b[38;2;255;85;151m" //hex 0xff5597
 #define ANSI_COLOR_RESET   "\x1b[0m"
 typedef struct cell{
-	int id;
 	char* charData;
 	int  numberData;
 	int isChar;
 	} cell;
 
-cell cellsArray[400];
+cell cellsArray[ROWS * COLUMNS];
 
 
 int cellpicker(int row, int column){
@@ -36,7 +37,6 @@ void cellModifier(int activeCell){
 	switch(numOrChar){
 		case 'N':
 		case 'n':
-		printf("case n");
 		printf("Input your data:");
 			scanf("%d", &intInput);
 			cellsArray[activeCell].numberData = intInput; 
@@ -45,7 +45,6 @@ void cellModifier(int activeCell){
 		
 		case 'C':
 		case 'c':
-		printf("case c");
 		printf("Input your data:");
 				scanf("%31s", charInput);
 		cellsArray[activeCell].charData = strdup(charInput);
@@ -53,7 +52,7 @@ void cellModifier(int activeCell){
 		break;
 
 		default:
-		printf("not case n or c");
+		printf("Invalid choice, please input either N or C\n");
 		break;
 	}
 
@@ -73,32 +72,41 @@ void cellReader(int activeCell){
 int main(void){
 	int activeRow;
 	int activeColumn;
-	for(int i = 0; i < 400; i++){
-		cellsArray[i].id = i;
-		cellsArray[i].charData = malloc(32 * sizeof(char)); //gives every cell a unique identifier, and allocates 32 bytes of memory for character data per cell
+	for(int i = 0; i < ROWS * COLUMNS; i++){
+		cellsArray[i].charData = malloc(32 * sizeof(char)); //Allocates 32 bytes of memory for character data per cell
 	}
 	while(1){
-	printf("Choose your row:");
+	printf("Choose your row (1-%d), or 0 to quit:", ROWS);
 		scanf("%d", &activeRow);
-	printf("Choose your column:");
+	if(activeRow == 0){
+		break;
+	}
+	if(activeRow < 1 || activeRow > ROWS) {
+		printf("Not a valid row, please choose a row between 1 and %d\n", ROWS);
+		continue;
+	}
+	printf("Choose your column(1-%d)", COLUMNS);
 		scanf("%d", &activeColumn);
+	
+	if(activeColumn < 1 || activeColumn > COLUMNS) {
+		printf("Not a valid column, please choose a column between 1 and %d\n", COLUMNS);
+		continue;
+	}
 		int activeCell = cellpicker(activeRow, activeColumn);
 //	printf("%d", activeCell); /*just making sure cellpicker works*/
 	char modifyOrRead; 
-	printf("What would you like to do? (m)odify value/(r)ead value/(q)uit program: ");
+	printf("What would you like to do? (m)odify value/(r)ead value");
 	scanf(" %c", &modifyOrRead);
 		modifyOrRead = tolower(modifyOrRead);
 			if (modifyOrRead != 'm' && modifyOrRead != 'r' && modifyOrRead != 'q'){
 
 			fprintf(stderr, "error, must enter m or r\n");
 				return -1;
+				break;
 			}
 			else if (modifyOrRead == ('m')){
 			cellModifier(activeCell);
 			
-			}
-			else if (modifyOrRead == ('q')){
-				break;
 			}
 			else{ 
 				cellReader(activeCell);
